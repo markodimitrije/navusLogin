da5cda12995ab8ba8001d9658e2d632a6c7c8683
Dimitrijevic Marko <markodimitrije@gmail.com>
2020-04-13 17:44:16 +0200

    localizable (not tested..)

--- navusLogin/Data/ErrorAlertInfoDictionaryFactory.swift
+++ navusLogin/Data/ErrorAlertInfoDictionaryFactory.swift
@@ -22,27 +22,32 @@ class ErrorAlertInfoDictionaryFactory {
     }
     
     static private func noConnectionPair() -> [String: Any] {
-        let alertInfo = getAlertInfo(titleKeyString: "", textKeyString: "")
+        let alertInfo = getAlertInfo(titleKeyString: "noConnection.title",
+                                     textKeyString: "noConnection.text")
         return [LoginError.noConnection.getHash(): alertInfo]
     }
     
     static private func unknownUserPair() -> [String: Any] {
-        let alertInfo = getAlertInfo(titleKeyString: "", textKeyString: "")
+        let alertInfo = getAlertInfo(titleKeyString: "unknownUser.title",
+                                     textKeyString: "unknownUser.text")
         return [LoginError.unknownUser.getHash(): alertInfo]
     }
     
     static private func emailNotValidPair() -> [String: Any] {
-        let alertInfo = getAlertInfo(titleKeyString: "", textKeyString: "")
+        let alertInfo = getAlertInfo(titleKeyString: "emailNotValid.title",
+                                     textKeyString: "emailNotValid.text")
         return [LoginValidationError.emailNotValid.getHash(): alertInfo]
     }
     
     static private func passNotValidPair() -> [String: Any] {
-        let alertInfo = getAlertInfo(titleKeyString: "", textKeyString: "")
+        let alertInfo = getAlertInfo(titleKeyString: "passNotValid.title",
+                                     textKeyString: "passNotValid.text")
         return [LoginValidationError.passNotValid.getHash(): alertInfo]
     }
     
     static private func fieldIsEmptyPair() -> [String: Any] {
-        let alertInfo = getAlertInfo(titleKeyString: "", textKeyString: "")
+        let alertInfo = getAlertInfo(titleKeyString: "fieldIsEmpty.title",
+                                     textKeyString: "fieldIsEmpty.text")
         return [LoginValidationError.fieldIsEmpty.getHash(): alertInfo]
     }
     
